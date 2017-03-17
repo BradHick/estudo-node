4bfeea4bac6e94c2fc3abc3fa93dfa8030408244
Brad Hick <bradleyhrm@gmail.com>
2017-03-17 14:52:28 -0300

    Organixando o código e dando o pattern DAO a uma parte do projeto

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.get('/produtos', function(req, res) {
 
     var connection = app.infra.connectionFactory();
-    var produtosBanco = new app.infra.produtosBanco(connection);
+    var produtosBanco = new app.infra.ProdutosDAO(connection);
 
     produtosBanco.lista(function(err,results) {
         res.render('produtos/lista',{lista:results});
@@ -15,7 +15,7 @@ module.exports = function(app) {
 
   app.get('/produtos/remove', function() {
     var connection = app.infra.connectionFactory();
-    var produtosBanco = app.infra.produtosBanco(connection);
+    var produtosBanco = app.infra.ProdutosDAO(connection);
 
     var produto = produtosBanco.carrega(id, callback);
     if(produto){
