36e4ff295b8f726ec66c0c73a9d16d8651172c25
Brad Hick <bradleyhrm@gmail.com>
2017-03-17 16:16:27 -0300

    Incluindo o Body-Parser

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,9 +3,9 @@ module.exports = function(app) {
   app.get('/produtos', function(req, res) {
 
     var connection = app.infra.connectionFactory();
-    var produtosBanco = new app.infra.ProdutosDAO(connection);
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-    produtosBanco.lista(function(err,results) {
+    produtosDAO.lista(function(err,results) {
         res.render('produtos/lista',{lista:results});
     });
 
@@ -13,15 +13,33 @@ module.exports = function(app) {
 
   });
 
-  app.get('/produtos/remove', function() {
-    var connection = app.infra.connectionFactory();
-    var produtosBanco = app.infra.ProdutosDAO(connection);
 
-    var produto = produtosBanco.carrega(id, callback);
-    if(produto){
-      produtosBanco.remove(produto, callback);
-    }
+app.get('/produtos/form', function(req, res) {
+  res.render('produtos/form');
+});
 
+app.post('produtos/salva', function(req,res) {
+  var produto = req.body;
+
+  var connection = app.infra.connectionFactory();
+  var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+
+  produtosDAO.salva(produto, function(erros, resultados) {
+    res.render('produtos/lista');
   });
+});
+
+
+  // app.get('/produtos/remove', function() {
+  //   var connection = app.infra.connectionFactory();
+  //   var produtosDAO = app.infra.ProdutosDAO(connection);
+  //
+  //   var produto = produtosBanco.carrega(id, callback);
+  //   if(produto){
+  //     produtosDAO.remove(produto, callback);
+  //   }
+  //
+  // });
 
 }
