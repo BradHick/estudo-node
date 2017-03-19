86d4a2598fe347016768bf367f39f17601c5e438
Brad Hick <bradleyhrm@gmail.com>
2017-03-19 18:02:12 -0300

    Ajusteando a função de salvar

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,33 +1,29 @@
 
 module.exports = function(app) {
-  app.get('/produtos', function(req, res) {
+  var listaProdutos = function(req,res){
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.lista(function(erros,resultados){
+        res.render('produtos/lista',{lista:resultados});
+    });
+    connection.end();
+};
+
+app.get('/produtos',listaProdutos);
+
+app.get('/produtos/form',function(req,res){
+    res.render('produtos/form');
+});
+
+app.post('/produtos/salva',function(req,res){
+
+    var produto = req.body;
 
     var connection = app.infra.connectionFactory();
     var produtosDAO = new app.infra.ProdutosDAO(connection);
-
-    produtosDAO.lista(function(err,results) {
-        res.render('produtos/lista',{lista:results});
+    produtosDAO.salva(produto,function(erros,resultados){
+        res.redirect('/produtos');
     });
-
-    connection.end();
-
-  });
-
-
-app.get('/produtos/form', function(req, res) {
-  res.render('produtos/form');
-});
-
-app.post('produtos/salva', function(req,res) {
-  var produto = req.body;
-
-  var connection = app.infra.connectionFactory();
-  var produtosDAO = new app.infra.ProdutosDAO(connection);
-
-
-  produtosDAO.salva(produto, function(erros, resultados) {
-    res.render('produtos/lista');
-  });
 });
 
 
--- config/express.js
+++ config/express.js
@@ -8,7 +8,7 @@ module.exports = function() {
   app.set('view engine', 'ejs');
   app.set('views', './app/views');
 
-  app.use(bodyParser.urlencoded({extended:true}));
+  app.use(bodyParser.urlencoded({extended: true}));
 
   load('routes', {cwd:'app'})
       .then('infra')
